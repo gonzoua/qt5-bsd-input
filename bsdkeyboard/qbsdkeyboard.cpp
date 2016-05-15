68797659bdf23ea922997fb8d7628b8593d96b33
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2016-05-15 16:36:33 -0700

    Fix copyrights and header file guards

--- bsdkeyboard/qbsdkeyboard.cpp
+++ bsdkeyboard/qbsdkeyboard.cpp
@@ -1,7 +1,8 @@
 /****************************************************************************
 **
-** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
-** Contact: http://www.qt-project.org/legal
+** Copyright (C) 2015 The Qt Company Ltd.
+** Copyright (C) 2015-2016 Oleksandr Tymoshenko <gonzo@bluezbox.com>
+** Contact: http://www.qt.io/licensing/
 **
 ** This file is part of the QtGui module of the Qt Toolkit.
 **
@@ -10,9 +11,9 @@
 ** Licensees holding valid commercial Qt licenses may use this file in
 ** accordance with the commercial license agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and Digia. For licensing terms and
-** conditions see http://qt.digia.com/licensing. For further information
-** use the contact form at http://qt.digia.com/contact-us.
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see http://www.qt.io/terms-conditions. For further
+** information use the contact form at http://www.qt.io/contact-us.
 **
 ** GNU Lesser General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU Lesser
@@ -23,17 +24,15 @@
 ** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
 ** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
 **
-** In addition, as a special exception, Digia gives you certain additional
-** rights. These rights are described in the Digia Qt LGPL Exception
+** As a special exception, The Qt Company gives you certain additional
+** rights. These rights are described in The Qt Company LGPL Exception
 ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
 **
 ** $QT_END_LICENSE$
 **
 ****************************************************************************/
 
-
 #include "qbsdkeyboard.h"
-
 
 #include <QSocketNotifier>
 #include <QStringList>
@@ -145,7 +144,6 @@ QBsdKeyboardHandler::QBsdKeyboardHandler(const QString &key,
         qWarning("Cannot open keyboard input device '%s': %s", (const char*)device.toLatin1(), strerror(errno));
     }
 }
-
 
 QBsdKeyboardHandler::~QBsdKeyboardHandler()
 {
@@ -480,7 +478,6 @@ void QBsdKeyboardHandler::switchLed(int led, bool state)
         return;
     }
 }
-
 
 void QBsdKeyboardHandler::unloadKeymap()
 {
